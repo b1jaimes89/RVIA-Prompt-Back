8210c7ff113391d45adab639e89b6a40bfbd01a7
hechicero5000 <hechicero5000@gmail.com>
2024-10-24 16:47:53 -0600

    Add files via upload
    Cambios prompt BD appModule y main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
-import { GptModule } from './gpt/gpt.module';
-import { BitoModule } from './bito/bito.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LenguajesModule } from './lenguajes/lenguajes.module';
+import { EsquemasModule } from './esquemas/esquemas.module';
+import { PromptsModule } from './prompts/prompts.module';
+import { LenguajesXPromptsModule } from './lenguajes-x-prompts/lenguajes-x-prompts.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    GptModule,
-    BitoModule,
-  ]
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize:false
+    }),
+    PromptsModule,            //si
+    LenguajesXPromptsModule,  //si
+    EsquemasModule,     //si
+    LenguajesModule,    //si
+  ],
 })
 export class AppModule {}
+  
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  
+
   // Habilita CORS
   app.enableCors({
     origin: '*', // Permite todas las URLs de origen
@@ -20,7 +21,18 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Example') // Cambia este título según tu API
+    .setDescription('Descripción de la API') // Cambia esta descripción según tu API
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // 'api' es la ruta en la que accederás a Swagger
+
   await app.listen(process.env.PORT);
-  logger.log(`-> App running on port ${ process.env.PORT }`);
+  logger.log(`-> App running on port ${process.env.PORT}`);
 }
-bootstrap();
+
+bootstrap();  
